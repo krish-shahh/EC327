146d02b9d8a4ffded51efe2d289b092706a1e198
Krish Shah <2003kshah@gmail.com>
2023-02-04 11:59:34 -0500

    update

--- Lab1/flip.cpp
+++ Lab1/flip.cpp
@@ -10,41 +10,59 @@ Run your program 2622 times and report the total number of Heads and Tails for e
 
 using namespace std;
 
-int flipCoin() {
-  return rand() % 2;
-}
+int main()
+{
+    int event1_heads = 0;
+    int event1_tails = 0;
+    int event2_heads = 0;
+    int event2_tails = 0;
 
-int main() {
-  srand(time(0));
+    srand(time(0));
 
-  int event1Heads = 0, event1Tails = 0, event2Heads = 0, event2Tails = 0;
+    for (int i = 0; i < 2622; i++)
+    {
+        int previous_flip = 0;
+        int current_flip = 0;
+        bool event1_found = false;
+        bool event2_found = false;
 
-  for (int i = 0; i < 2622; i++) {
-    int prev = flipCoin();
-    int current = flipCoin();
+        while (!event1_found && !event2_found)
+        {
+            previous_flip = current_flip;
+            current_flip = rand() % 2;
 
-    if (prev == 0 && current == 1) {
-      event1Heads++;
-      event1Tails++;
-    } else {
-      event1Heads += prev;
-      event1Tails += (current == 0 ? 1 : 0);
-      prev = current;
-      current = flipCoin();
-
-      if (prev == 0 && current == 0) {
-        event2Heads += prev;
-        event2Tails += current + 2;
-      } else {
-        event2Heads += prev;
-        event2Tails += (current == 0 ? 1 : 0);
-      }
+            if (previous_flip == 0 && current_flip == 1)
+            {
+                event1_found = true;
+                event1_heads++;
+                event1_tails++;
+            }
+            else if (previous_flip == 1 && current_flip == 1)
+            {
+                event2_found = true;
+                event2_heads++;
+                event2_tails++;  // add this line
+            }
+            else
+            {
+                if (current_flip == 0)
+                {
+                    event1_heads++;
+                    event2_heads++;
+                }
+                else
+                {
+                    event1_tails++;
+                    event2_tails++;
+                }
+            }
+        }
     }
-  }
 
-  cout << "Event 1: " << event1Heads << " Heads and " << event1Tails << " Tails" << endl;
-  cout << "Event 2: " << event2Heads << " Heads and " << event2Tails << " Tails" << endl;
+    cout << "Event 1 total heads: " << event1_heads << endl;
+    cout << "Event 1 total tails: " << event1_tails << endl;
+    cout << "Event 2 total heads: " << event2_heads << endl;
+    cout << "Event 2 total tails: " << event2_tails << endl;
 
-  return 0;
+    return 0;
 }
-
