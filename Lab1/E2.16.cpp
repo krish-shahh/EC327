650a73df472a7d9986043ea781eafbfd245f45cd
Krish Shah <2003kshah@gmail.com>
2023-02-04 12:13:18 -0500

    new E2.16

--- Lab1/E2.16.cpp
+++ Lab1/E2.16.cpp
@@ -4,43 +4,27 @@
 */
 
 #include <iostream>
-#include <cstdlib>
-
-void time_difference(std::string time1, std::string time2) {
-    // Convert the times to minutes
-    int minutes1 = stoi(time1.substr(0, 2)) * 60 + stoi(time1.substr(2));
-    int minutes2 = stoi(time2.substr(0, 2)) * 60 + stoi(time2.substr(2));
-    
-    // Calculate the difference in minutes
-    int difference = 0;
-    if (minutes2 >= minutes1) {
-        difference = minutes2 - minutes1;
-    } else {
-        difference = 1440 - (minutes1 - minutes2); // 1440 (24*60) is the total number of minutes in a day
-    }
-    
-    // Convert the difference to hours and minutes
-    int hours = difference / 60;
-    int minutes = difference % 60;
-    
-    // Print the result
-    std::cout << hours << " hours and " << minutes << " minutes" << std::endl;
-}
+#include <chrono>
+#include <ctime>
 
 int main() {
-    std::string time1, time2;
-    
-    std::cout << "Enter the first time (in military format): ";
-    std::cin >> time1;
-    
-    std::cout << "Enter the second time (in military format): ";
-    std::cin >> time2;
-    
-    time_difference(time1, time2);
-    
+    int firstTime, secondTime;
+    std::cout << "Enter first time (in military format): ";
+    std::cin >> firstTime;
+    std::cout << "Enter second time (in military format): ";
+    std::cin >> secondTime;
+
+    int duration;
+    if (firstTime > secondTime) {
+        duration = 2400 - (firstTime - secondTime);
+    } else {
+        duration = secondTime - firstTime;
+    }
+
+    int hours = duration / 100;
+    int minutes = duration % 100;
+
+    std::cout << "The duration between the times is " << hours << " hours and " << minutes << " minutes." << std::endl;
+
     return 0;
 }
-
-
-
-
