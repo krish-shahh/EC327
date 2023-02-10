34407f3cbea660cff4310ebdb2267d8d013e3b9a
Krish Shah <2003kshah@gmail.com>
2023-02-10 00:42:31 -0500

    hw 1 update

--- HW1/Problem0.cpp
+++ HW1/Problem0.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 #include <limits>
 
+using namespace std;
+
 int main() {
-    std::cout << "unsigned char min: " << (int)std::numeric_limits<unsigned char>::min() << std::endl;
-    std::cout << "unsigned char max: " << (int)std::numeric_limits<unsigned char>::max() << std::endl;
-    std::cout << "unsigned char size: " << sizeof(unsigned char) << " bytes" << std::endl;
-    std::cout << "unsigned int min: " << std::numeric_limits<unsigned int>::min() << std::endl;
-    std::cout << "unsigned int max: " << std::numeric_limits<unsigned int>::max() << std::endl;
-    std::cout << "unsigned int size: " << sizeof(unsigned int) << " bytes" << std::endl;
-    std::cout << "unsigned long min: " << std::numeric_limits<unsigned long>::min() << std::endl;
-    std::cout << "unsigned long max: " << std::numeric_limits<unsigned long>::max() << std::endl;
-    std::cout << "unsigned long size: " << sizeof(unsigned long) << " bytes" << std::endl;
-    std::cout << "float min: " << std::numeric_limits<float>::min() << std::endl;
-    std::cout << "float max: " << std::numeric_limits<float>::max() << std::endl;
-    std::cout << "float size: " << sizeof(float) << " bytes" << std::endl;
-    std::cout << "double min: " << std::numeric_limits<double>::min() << std::endl;
-    std::cout << "double max: " << std::numeric_limits<double>::max() << std::endl;
-    std::cout << "double size: " << sizeof(double) << " bytes" << std::endl;
-    return 0;
+    cout << "unsigned char min: " << (int) numeric_limits<unsigned char>::min() <<  endl;
+    cout << "unsigned char max: " << (int) numeric_limits<unsigned char>::max() <<  endl;
+    cout << "unsigned char size: " << sizeof(unsigned char) << " bytes" <<  endl;
+    cout << "unsigned int min: " <<  numeric_limits<unsigned int>::min() <<  endl;
+    cout << "unsigned int max: " <<  numeric_limits<unsigned int>::max() <<  endl;
+    cout << "unsigned int size: " << sizeof(unsigned int) << " bytes" <<  endl;
+    cout << "unsigned long min: " <<  numeric_limits<unsigned long>::min() <<  endl;
+    cout << "unsigned long max: " <<  numeric_limits<unsigned long>::max() <<  endl;
+    cout << "unsigned long size: " << sizeof(unsigned long) << " bytes" <<  endl;
+    cout << "float min: " <<  numeric_limits<float>::min() <<  endl;
+    cout << "float max: " <<  numeric_limits<float>::max() <<  endl;
+    cout << "float size: " << sizeof(float) << " bytes" <<  endl;
+    cout << "double min: " <<  numeric_limits<double>::min() <<  endl;
+    cout << "double max: " <<  numeric_limits<double>::max() <<  endl;
+    cout << "double size: " << sizeof(double) << " bytes" <<  endl;
+  return 0;
 }
